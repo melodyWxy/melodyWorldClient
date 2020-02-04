1d98eef66ef74dbf3a69bee18b9795c6709e5f89
melodyWxy <18210711176@163.com>
2020-02-04 18:42:57 +0800

    +取消跨域

--- src/utils/xFetch.js
+++ src/utils/xFetch.js
@@ -1,11 +1,13 @@
-export  async function xGet(...props){
-    const result = await fetch(...props)
+const hostname = "http://120.27.61.111:8806";
+// const hostname = "http://localhost:8806";
+export  async function xGet(url){
+    const result = await fetch(hostname+url)
         .then(res=>res.json())
         .catch(console.error)
     return result;
 }
 export async function xPost(url,body){
-    const result = await fetch(url,{
+    const result = await fetch(hostname+url,{
           method: "POST",
           headers:{
             "Content-Type":"application/json"
